d093180d711f18d9343bb840ad7d430e40ef81a8
LoganArnett <Loganarn5@gmail.com>
2019-01-11 14:01:51 -0500

    process .env

--- server.js
+++ server.js
@@ -101,6 +101,6 @@ app.get("/scrape", function(req, res) {
 // });
 
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("App running on port 3000!");
 });
