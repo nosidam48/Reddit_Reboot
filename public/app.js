5e709f8ade4cf85cc3267d6920379edb3546951c
nosidam48 <madison.tift68@gmail.com>
2019-01-11 14:54:14 -0500

    change user

--- public/app.js
+++ public/app.js
@@ -16,7 +16,7 @@ getResults()
 
 $("#saved").on("click", function() {
   //GET route to /saved to display 
-  console.log("bubble");
+  console.log("asdfawe");
   
 })
 $("#clear").on("click", function() {
